99708cfac4c38f85f558714e6dbe2e0f7a71742d
noweymik <22000136@handong.edu>
2022-10-28 17:29:32 -0500

    first commit

---
+++ Day1.cpp
@@ -0,0 +1,13 @@
+// Baekjoon Q.1000 
+// A+B
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+    cout << A+B;
+
+    return 0;
+}
---
+++ Day3.cpp
@@ -0,0 +1,12 @@
+// Backjoon Q.1008
+// A/B
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+    printf("%.9lf", (double)A/(double)B);
+    return 0;
+}
