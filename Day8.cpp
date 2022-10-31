3f71dbb0f65c5a39ceb2eb831938b24e0860359d
noweymik <22000136@handong.ac.kr>
2022-10-31 17:45:20 -0500

    fourth commit

---
+++ Day8.cpp
@@ -0,0 +1,22 @@
+// Baekjoon Q.25304
+// 영수증
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int total = 0, cal_total = 0;
+    int a, b, N = 0;
+
+    cin >> total;
+    cin >> N;
+
+    for(int i=0; i<N; i++) {
+        cin >> a >> b;
+        cal_total += (a*b);
+    }
+    if(total == cal_total) cout << "Yes";
+    else cout << "No";
+
+    return 0;
+}
