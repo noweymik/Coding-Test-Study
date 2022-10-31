c0181865995f511aaadaaaa76d7d4cbae29f3707
noweymik <22000136@handong.edu>
2022-10-30 23:56:21 -0500

    third commit

---
+++ Day7.cpp
@@ -0,0 +1,16 @@
+// Baekjoon Q.9498
+// 시험 성적
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int score;
+    cin >> score;
+    if(score >=90 && score <=100) cout << "A";
+    else if(score >=80) cout << "B";
+    else if(score >=70) cout << "C";
+    else if(score >=60) cout << "D";
+    else cout << "F";
+    return 0;
+}
