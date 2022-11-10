3798c571e3ec546427e58f0ed4d41b91fbe1deea
noweymik <22000136@handong.ac.kr>
2022-11-10 01:49:18 -0600

    Create Day 35,36

---
+++ 2022/Day35.cpp
@@ -0,0 +1,14 @@
+// Baekjoon Q.10950
+// A+B - 3
+#include <iostream>
+using namespace std;
+
+int main() {     
+    int T, a, b;
+    cin >> T;
+    for(int i=0; i<T; i++) {
+        cin >> a >> b;
+        cout << a+b << endl;
+    }
+    return 0;
+}
---
+++ 2022/Day36.cpp
@@ -0,0 +1,14 @@
+// Baekjoon Q.10952
+// A+B - 5
+#include <iostream>
+using namespace std;
+
+int main() {     
+    int a,b;
+    while(1) {
+        cin >> a >> b;
+        if(a == 0 && b == 0) break;
+        cout << a+b << endl;
+    }
+    return 0;
+}
