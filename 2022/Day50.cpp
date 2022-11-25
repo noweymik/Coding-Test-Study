59c00d87bf18fc129ae53d0d026397727f7e9158
noweymik <22000136@handong.ac.kr>
2022-11-25 01:15:27 -0600

    Update Day50.cpp

--- 2022/Day50.cpp
+++ 2022/Day50.cpp
@@ -2,6 +2,15 @@
 // 팩토리얼
 #include <iostream>
 using namespace std;
+
+int calc(int n);
+
+int main() {     
+    int n;
+    cin >> n;
+    cout << calc(n);
+    return 0;
+}
 int calc(int n) {    
     if(n > 0) {
         return n * calc(n-1);
@@ -9,10 +18,4 @@ int calc(int n) {
     else {
         return 1;
     }
-}
-int main() {     
-    int n;
-    cin >> n;
-    cout << calc(n);
-    return 0;
 }
