7a1cc3922cac440e9b805a15d279ca31d98c63d8
noweymik <22000136@handong.ac.kr>
2022-11-28 01:10:27 -0600

    Create Day53.cpp

---
+++ 2022/Day53.cpp
@@ -0,0 +1,20 @@
+// Baekjoon Q.10039
+// 평균 점수
+#include <iostream>
+using namespace std;
+
+int main() {     
+    int score[5];
+    int total = 0;
+
+    for(int i=0; i<5; i++) {
+        cin >> score[i];
+        if(score[i] < 40) {
+            score[i] = 40;
+        }
+        total = total + score[i];
+    }   
+
+    cout << total / 5;
+    return 0;
+}
