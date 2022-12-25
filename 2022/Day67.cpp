258b92c68cac43d0eb62dc8cdd265e838202bb27
noweymik <22000136@handong.ac.kr>
2022-12-25 03:42:07 -0500

    Update Day67.cpp

--- 2022/Day67.cpp
+++ 2022/Day67.cpp
@@ -4,5 +4,16 @@
 using namespace std;
 
 int main() {  
-    
+    int N;
+    long long list[45];
+
+    cin >> N;
+
+    list[0] = 0;
+    list[1] = 1;
+
+    for(int i=2; i<=N; i++) {
+        list[i] = list[i-1] + list[i-2];
+    }
+    cout << list[N];
 }
