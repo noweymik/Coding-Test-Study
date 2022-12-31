07fc7fe59b1b69c0d59efc62999d9a5f888dae81
noweymik <22000136@handong.ac.kr>
2022-12-31 14:56:29 -0500

    Update Day68.cpp

--- 2022/Day68.cpp
+++ 2022/Day68.cpp
@@ -1,40 +1,11 @@
 // Baekjoon Q.1550
-// 16진수 -> 고치기
+// 16진수
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 int main() {  
-    string input; // Hexadecimal
     int num;
-    int decimal = 0;
-
-    cin >> input;
-
-    for(int i=1; i<=input.length(); i++) {
-        
-        if(input[i] == 'A') {
-            decimal += 10 * pow(16, input.length()-i);
-        }
-        else if(input[i] == 'B'){
-            decimal += 11 * pow(16, input.length()-i);
-        }
-        else if(input[i] == 'C'){
-            decimal += 12 * pow(16, input.length()-i);
-        }
-        else if(input[i] == 'D'){
-            decimal += 13 * pow(16, input.length()-i);
-        }
-        else if(input[i] == 'E'){
-            decimal += 14 * pow(16, input.length()-i);
-        }
-        else if(input[i] == 'F'){
-            decimal += 15 * pow(16, input.length()-i);
-        }
-        else {
-            decimal += input[i] * pow(16, input.length()-i);
-        }
-    }
-
-    cout << decimal;
+    scanf("%x", &num);
+    printf("%d", num);
+    return 0;
 }
