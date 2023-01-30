0267d67d7061c9186066221e60b61f9757e9bdb6
Yewon Kim <72730302+noweymik@users.noreply.github.com>
2023-01-30 18:25:13 +0900

    Create Day5.cpp

---
+++ 2023/Day5.cpp
@@ -0,0 +1,14 @@
+// Baekjoon Q.2741 
+// N 찍기
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    for(int i=1; i<=N; i++) {
+        printf("%d\n", i);
+    }
+    return 0;
+}
