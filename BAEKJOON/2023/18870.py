512dc7b5dd91439fdbe4d99cf82e641d2d4c2b71
Yewon Kim <72730302+noweymik@users.noreply.github.com>
2023-11-06 13:59:35 +0900

    23/11/06

---
+++ BAEKJOON/2023/18870.py
@@ -0,0 +1,16 @@
+# 백준 18870번  
+# 좌표 압축
+# 배운 점 : 중복 제거 - set 이용, index와 같이 반복 - enumerate 
+N = int(input())
+
+X = list(map(int, input().split()))
+X_set = sorted(set(X))
+
+dict = {num : index for index, num in enumerate(X_set)}
+
+for i in X:
+    print(dict[i], end = " ")
+    
+            
+    
+
