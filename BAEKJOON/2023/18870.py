865739ae525336c0cc9f13c15e4b56fb37a8b352
Yewon Kim <72730302+noweymik@users.noreply.github.com>
2023-11-07 20:45:59 +0900

    Update 18870.py

--- BAEKJOON/2023/18870.py
+++ BAEKJOON/2023/18870.py
@@ -1,6 +1,7 @@
 # 백준 18870번  
 # 좌표 압축
 # 배운 점 : 중복 제거 - set 이용, index와 같이 반복 - enumerate 
+
 N = int(input())
 
 X = list(map(int, input().split()))
@@ -11,6 +12,3 @@ dict = {num : index for index, num in enumerate(X_set)}
 for i in X:
     print(dict[i], end = " ")
     
-            
-    
-
