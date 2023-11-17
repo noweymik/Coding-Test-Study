e7b2e118967f90fcf0faaf3626bd1d1a69b977b5
Yewon Kim <72730302+noweymik@users.noreply.github.com>
2023-11-17 12:02:10 +0900

    23/11/17

--- BAEKJOON/2023/5073.py
+++ BAEKJOON/2023/5073.py
@@ -3,20 +3,20 @@
 
 while True:
     x, y, z = map(int, input().split())
-    if x==0 and y==0 and z==0:
+    if x == 0 and y == 0 and z == 0:
         break
     
-    if x==y and y==z:
+    list = [x, y, z]
+    list.sort(reverse=True)
+    
+    if list[0] >= (list[1] + list[2]):
+        print("Invalid")
+        
+    elif x == y == z:
         print("Equilateral")
     
     elif x==y or y==z or z==x:
         print("Isosceles")
 
     else:
-        list = [x, y, z]
-        list.sort(reverse=True)
-        
-        if list[0] >= (list[1] + list[2]):
-            print("Invalid")
-        else:
-            print("Scalene")
+        print("Scalene")
