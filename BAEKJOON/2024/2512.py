fd0194e6e220d8f7394ca8a2415ac4b12d199762
Yewon Kim <72730302+noweymik@users.noreply.github.com>
2024-01-17 22:38:32 +0900

    24/1/17-updated

--- BAEKJOON/2024/2512.py
+++ BAEKJOON/2024/2512.py
@@ -11,12 +11,12 @@ total = int(input())
 start, end = 0, max(budget)
 
 while start <= end:
-    mid = (start+end) // 2
+    mid = (start+end) // 2 # 몫
     sum = 0
     for b in budget:
         sum += min(b, mid)
                 
-    if sum <= total:
+    if sum <= total: # 한도를 넘지 않는 경우
         start = mid + 1
     else:
         end = mid - 1
