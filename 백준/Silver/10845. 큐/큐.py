36e3fd17e12bc01e667afc2428ced06908655285
Yewon Kim <72730302+noweymik@users.noreply.github.com>
2024-06-06 20:39:50 +0900

    [Silver IV] Title: 큐, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,38 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+queue = []
+
+for i in range(N):
+    command = list(input().split())
+    
+    if command[0] == 'push':
+        queue.append(command[1])
+        
+    elif command[0] == 'pop':
+        if queue:            
+            print(queue.pop(0))
+        else:
+            print(-1)
+            
+    elif command[0] == 'size':
+        print(len(queue))
+        
+    elif command[0] == 'empty':
+        if queue:
+            print(0)
+        else:
+            print(1)   
+
+    elif command[0] == 'front':
+        if queue:
+            print(queue[0])
+        else:
+            print(-1)
+            
+    elif command[0] == 'back':
+        if queue:
+            print(queue[-1])
+        else:
+            print(-1)      
