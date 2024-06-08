0e9f1e31ed7fbb96ab0977882c6c499df2407f11
Yewon Kim <72730302+noweymik@users.noreply.github.com>
2024-06-08 22:36:03 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 64.90 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,11 +1,11 @@
-def solution(arr):    
-    # first in last out
-    stack = []    
+def solution(arr): 
+    stack = []
     for a in arr:
-        # stack이 empty하지 않고 마지막 원소가 a가 아닌 경우
-        if stack and stack[-1] == a:
+        if stack:
+            if stack[-1] == a:
                 continue
-        # stack이 empty한 경우거나 마지막 원소가 a가 아닌 경우
+            else:
+                stack.append(a)
         else:
-            stack.append(a)    
+            stack.append(a)       
     return stack
