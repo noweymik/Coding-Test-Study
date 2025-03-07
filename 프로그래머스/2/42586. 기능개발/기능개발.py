ed2b3451cb49d6640b91ee22032050261ebed94c
Yewon Kim <noweymik01@gmail.com>
2025-03-07 13:17:06 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,23 +1,22 @@
-def solution(progresses, speeds):     
-    days = [0] * len(progresses)
-            
-    # progresses 개수만큼 반복해서 각 progress마다 걸리는 작업일(days) 계산
-    for i in range(len(progresses)):
-        while True:
-            if progresses[i] >= 100:
-                break
-            progresses[i] += speeds[i]
-            days[i] += 1
-                    
+import math
+
+def solution(progresses, speeds): 
     answer = []
+    days = []    
+    for i, p in enumerate(progresses):                
+        days.append(math.ceil((100 - p) / speeds[i]))    
     
-    while days:          
-        top = days.pop(0)
-        count = 1   # 배포될 수 있는 기능 개수
-        
-        # top과 days의 첫번째 원소를 비교 (top이 높으면 같이 배포 가능하니 pop)
-        while days and top >= days[0]:             
-            days.pop(0) 
+    top = days.pop(0)
+    count = 1
+    while days:        
+        if top >= days[0] and days:
+            days.pop(0)
             count += 1
-        answer.append(count)                
+        else:
+            answer.append(count)
+            count = 0
+            if days:            
+                top = days.pop(0)
+                count = 1
+    answer.append(count)
     return answer
